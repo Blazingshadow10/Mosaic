4ef72b12ef7ec4badedaa2a66e1f1294538216fb
Blazingshadow10 <61059862+Blazingshadow10@users.noreply.github.com>
2022-03-13 23:47:21 -0500

    Update Mosaic.java

--- Mosaic.java
+++ Mosaic.java
@@ -20,11 +20,50 @@ class ABCTiles extends JPanel implements MouseListener {
     int objectDraw;
     private Face face;
 
+    public String getLetter() {
+        return letter;
+    }
+    public void setLetter(String s) {
+        letter = s;
+    }
+    public int getR() {
+        return r;
+    }
+    public int getG() {
+        return g;
+    }
+    public int getB() {
+        return b;
+    }
+    public void setRGB(int x, int y, int z) {
+        r=x;
+        g=y;
+        b=z;
+    }
+    public int getObjectDraw() {
+        return objectDraw;
+    }
+    public void setObjectDraw(int n) {
+        objectDraw = n;
+    }
+
+    // Basic constructor
     ABCTiles() {
         super();
         RandomVariable();
     }
 
+    // 2rd Constructor to set all values you want
+    ABCTiles(int r, int g, int b, String letter, int objectDraw) {
+        super();
+        face = new Face(getHeight(),getWidth());
+        setRGB(r,g,b);
+        setLetter(letter);
+        setObjectDraw(objectDraw);
+        this.addMouseListener(this);
+    }
+
+    // set all the random Values for the tile
     final public void RandomVariable() {
         r = RandomNum(0,255);
         g = RandomNum(0,255);
@@ -40,8 +79,6 @@ class ABCTiles extends JPanel implements MouseListener {
         this.addMouseListener(this);
     }
 
-    
-
     private static int RandomNum(int min, int max) {
         Random ran = new Random();
         return min + ran.nextInt(max-min+1);
@@ -51,6 +88,7 @@ class ABCTiles extends JPanel implements MouseListener {
         return ((colorIn+128)%256);
     }
 
+    // Prints data for the object
     public String toString() {
         String shape = " ";
         String s;
@@ -87,7 +125,7 @@ class ABCTiles extends JPanel implements MouseListener {
 
         final int fontSize=50;
         e.setFont(new Font("TimesRoman", Font.PLAIN, fontSize));
-        int positionX = (tileWidth/3);
+        int positionX = (tileWidth/2)-20;
         int positionY = (tileHeight/2)+20;
         e.drawString(letter,positionX,positionY);
 
@@ -96,6 +134,7 @@ class ABCTiles extends JPanel implements MouseListener {
         }
     }
 
+    // mouse click override (It complained when I did not have all of them)
     @Override
     public void mouseClicked(MouseEvent e) {
         objectDraw = 3;
@@ -126,31 +165,36 @@ class MosaicFrame extends JFrame implements ActionListener {
     private ArrayList<ABCTiles> tileList = new ArrayList<ABCTiles>();
 
     public MosaicFrame() {
-        setBounds(100,50,1200,800);
+        setBounds(100,50,1000,800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
 
+        // Panel to hold the button
         JPanel reRollPanel = new JPanel();
         contentPane.add(reRollPanel, BorderLayout.SOUTH);
 
+        // Makes a ReRoll button
         JButton reRoll = new JButton("Reroll");
         reRollPanel.add(reRoll);
         reRoll.addActionListener(this);
 
+        // Makes a new JPanel to hold all the tiles in a 12x12 grid
         JPanel ABCTiles = new JPanel();
         contentPane.add(ABCTiles, BorderLayout.CENTER);
         ABCTiles.setLayout(new GridLayout(12,12));
         
+        // Makes all the tiles and adds them to the grid.
         for(int i=0; i<144; i++) {
-            ABCTiles tile = new ABCTiles();
+            ABCTiles tile = new ABCTiles(255,100,100,"A",1);
             tileList.add(tile);
             ABCTiles.add(tile);
             System.out.println((i+1) + ": "+ tile);
         }
     }
 
+    // Logic for the reroll button
     public void actionPerformed(ActionEvent e) {
         int count = 1;
         for(ABCTiles tile : tileList) {
